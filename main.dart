beb92bd4a255421fa693b7dccf2fdbd5f9c9be3a
Steve <ycsteven.tsao@gmail.com>
2023-01-23 16:14:13 +0800

    if practive

--- main.dart
+++ main.dart
@@ -1,6 +1,20 @@
 // 開始的進入點
 main() {
-  sample(y: '線上教室', x: 'HKT ');
+  int avgWind = 0;
+
+  avgWind = 7;
+  isCallOffSchoolAndWork(avgWind);
+
+  avgWind = 4;
+  isCallOffSchoolAndWork(avgWind);
+}
+
+void isCallOffSchoolAndWork(int avgWind) {
+  if (avgWind >= 7) {
+    print('平均風速「已達」停班停課標準，\n請做好防颱準備。\n');
+  } else {
+    print('平均風速「未達」停班停課標準，\n早點睡請準時上班上課。\n');
+  }
 }
 
 void sample({x, y}) {
