b001362f61247e77333b0a3ede9bc7b5ecbf7a19
Steve <ycsteven.tsao@gmail.com>
2023-01-23 17:33:39 +0800

    class

--- main.dart
+++ main.dart
@@ -1,22 +1,12 @@
-// 開始的進入點
-main() {
-  int avgWind = 0;
-
-  avgWind = 7;
-  isCallOffSchoolAndWork(avgWind);
-
-  avgWind = 4;
-  isCallOffSchoolAndWork(avgWind);
+void main() {
+  DemoClass d1 = new DemoClass();
+  print(d1.intVal);
 }
 
-void isCallOffSchoolAndWork(int avgWind) {
-  if (avgWind >= 7) {
-    print('平均風速「已達」停班停課標準，\n請做好防颱準備。\n');
-  } else {
-    print('平均風速「未達」停班停課標準，\n早點睡請準時上班上課。\n');
+class DemoClass {
+  late int intVal;
+
+  DemoClass() {
+    intVal = -1;
   }
-}
-
-void sample({x, y}) {
-  print(x + y);
 }
